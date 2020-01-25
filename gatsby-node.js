d478343beb24b510509bace6f3d6def2c4ee7d6b
BekahHW <github email address>
2020-01-25 16:06:40 -0500

    Add gatsby-node.js and about pages

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+// Replacing '/' would result in empty string which is invalid
+const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+
+  const oldPage = Object.assign({}, page)
+  // Remove trailing slash unless page is /
+  page.path = replacePath(page.path)
+  if (page.path !== oldPage.path) {
+    // Replace new page with old page
+    deletePage(oldPage)
+    createPage(page)
+  }
+}
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const AboutPage = () => (
+  <main>
+    <h1>About the Author</h1>
+    <p>Bekah Hawrot Weigel is a mom to four and a trauma survivor.</p>
+  </main>
+)
+
+export default AboutPage
