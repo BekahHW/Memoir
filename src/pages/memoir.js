2b591941fc3073ac520aef1b4245f53d5a9870cc
BekahHW <github email address>
2020-02-01 06:39:24 -0500

    Add memoir page

---
+++ src/pages/memoir.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const MemoirPage = () => (
+  <main>
+    <h1>The Scars that Make Us Beautiful</h1>
+    <p>From that moment of intense pain of my organs rupturing, my grip on reality was lost. Everything I knew about myself, who I was, who I would be was gone. I begged the doctors and nurses to find what was wrong, but told me that I was ok and sent me home.</p>
+
+<p>When I went back to see my OBGYN days later with the same problem, she said I wasn't her "problem." Overwhelmed by shame, fear, and the unknown, I had to demand through tears that someone take my problems seriously. And when they finally did, they found that I had a vesicouterine fistula--a tunnel between my uterus and bladder--that formed when my uterus ruptured into my bladder. And that doctor who diagnosed me, he told me to go back to the OBGYN who told me I wasn't her problem. I didn't. My mom pleaded with a former high school classmate who was an OBGYN to see me. From there I was referred to another set of doctors for a series of tests, and ultimately the surgery that would put me in the hospital for a week, see me overdose on morphine and have to be treated with Narcan, and that would make me wish for death.</p>
+
+<p>I could never be the same after that. Plagued by depression, anxiety, PTSD, weight gain, and an inability to regain physical and mental health, I went to therapy, I took medication, but ultimately, it was  learning to code and finding community that helped me to heal and allowed my brain to stop cycling the moment that doctor told me that I wasn't her problem. The anger from that moment fueled my goal to talk to hospital administrators, which I did on two separate occasions, once to tell my story and once to help shape the new OBGYN clinic they would be opening. </p>
+  </main>
+)
+
+export default MemoirPage
